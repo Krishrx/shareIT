ec83092f6ff85e081c63a055281a4885bc9f2356
Krishrx <kalikrishnas342@gmail.com>
2023-11-24 00:31:18 +0530

    made db connection through mongoose sample api request is also made.

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,58 @@
+require('dotenv').config();
 const express = require("express");
-const app = express();
-const port = 8000;
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/blogs", (req, res) => {
-    res.json("hello from backend");
+const ThoughtsModel = require("./models/thoughtsModel");
+const port = 8000;
+const dbURI = process.env.MONGO_DB_URI;
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(port, () => {
+        console.log(`app listening on ${port}`);
+    })
+}).catch((err) => {
+    console.log(err);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("got the data")
 })
 
-app.listen(port, () => {
-    console.log(`app listening on ${port}`);
+
+
+app.get("/", (req, res) => {
+    const content = new ThoughtsModel({
+        "title": "food",
+        "content": "i like dosa",
+        "isPublic": false
+    });
+    content.save().then(result => {
+        console.log('Data saved:', result);
+        res.send("data saved");
+    })
+    .catch(error => {
+        console.error('Error saving data:', error);
+    });
 })
+
+app.get('/thoughts', (req, res) => {
+    ThoughtsModel.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch(error => {
+            console.error('Error while retrieving data:', error);
+            res.status(500).send('Internal Server Error');
+        });
+});
---
+++ backend/models/thoughtsModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const thoughtsSchema = new Schema({
+    title: {
+        type: String,
+        required:true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    isPublic: {
+        type: Boolean,
+        required: true,
+    }
+}, { timestamps: true })
+
+const Thoughts = mongoose.model("Thought", thoughtsSchema);
+
+module.exports = Thoughts;
